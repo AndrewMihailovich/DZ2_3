edb404f1427c2d54c6770efe56075c2d14e88c1f
Andrew <Andy>
2023-08-09 22:50:22 +0300

    DZ3

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+if (chislo < 1)
+{
+ Console.Write("Введите другое число от 1 до 7");
+}
+else if (chislo > 7)
+{
+ Console.Write("Введите другое число от 1 до 7");
+}
+ else if (chislo > 5 && chislo < 8)
+ {
+   Console.Write("да, это выходной"); 
+}
+ else
+{
+    Console.Write("нет, это рабочий день");
+}
